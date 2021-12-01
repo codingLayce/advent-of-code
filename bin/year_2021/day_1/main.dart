c17cf0bb9ae8157f3319ebf19d63a13c30fec1f0
Guilbert Lucas <lucas.guilbert@orange.com>
2021-12-01 09:21:43 +0100

    Solving Day 1 of 2021

---
+++ bin/year_2021/day_1/main.dart
@@ -0,0 +1,40 @@
+import '../../util.dart';
+
+int main(List<String> args) {
+  if (args.length != 1) {
+    return 1;
+  }
+
+  List<int> data = readIntData(args[0]);
+
+  processPuzzle(1, () => resolve1(data));
+
+  processPuzzle(2, () => resolve2(data));
+
+  return 0;
+}
+
+int resolve1(List<int> data) {
+  int count = 0;
+
+  for (int i = 1; i < data.length; i++) {
+    if (data[i] > data[i - 1]) count++;
+  }
+
+  return count;
+}
+
+int resolve2(List<int> data) {
+  int count = 0;
+  int sum = double.maxFinite.toInt();
+
+  for (int i = 2; i < data.length; i++) {
+    int curSum = data[i] + data[i - 1] + data[i - 2];
+    if (curSum > sum) {
+      count++;
+    }
+    sum = curSum;
+  }
+
+  return count;
+}
