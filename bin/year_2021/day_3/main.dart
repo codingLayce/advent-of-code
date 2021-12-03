5a02390612fa4477fc2466855ffd6a60cb219edf
Guilbert Lucas <lucas.guilbert@orange.com>
2021-12-03 09:47:22 +0100

    Simple and dirty (less than the first part) part 2

--- bin/year_2021/day_3/main.dart
+++ bin/year_2021/day_3/main.dart
@@ -54,5 +54,36 @@ Future<int> resolve1(List<String> data) async {
 }
 
 Future<int> resolve2(List<String> data) async {
-  return 0;
+  int oxygenGenratorRating = getOxygenGeneratorRating(List.from(data), 0);
+  int co2ScrubberRating = getCo2ScrubberRating(List.from(data), 0);
+
+  return oxygenGenratorRating * co2ScrubberRating;
+}
+
+int getCo2ScrubberRating(List<String> data, int index) {
+  int leastCommonBit = findLeastCommonBit(data, index);
+  data.removeWhere((element) => element[index] != "$leastCommonBit");
+  if (data.length == 1) return int.parse(data.single, radix: 2);
+  return getCo2ScrubberRating(data, index + 1);
+}
+
+int getOxygenGeneratorRating(List<String> data, int index) {
+  int mostCommonBit = findMostCommonBit(data, index);
+  data.removeWhere((element) => element[index] != "$mostCommonBit");
+  if (data.length == 1) return int.parse(data.single, radix: 2);
+  return getOxygenGeneratorRating(data, index + 1);
+}
+
+int findLeastCommonBit(List<String> data, int index) {
+  return findMostCommonBit(data, index) == 1 ? 0 : 1;
+}
+
+int findMostCommonBit(List<String> data, int index) {
+  int countSet = 0;
+
+  for (String line in data) {
+    if (line[index] == "1") countSet++;
+  }
+
+  return countSet >= data.length / 2 ? 1 : 0;
 }
