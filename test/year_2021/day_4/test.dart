d817bb5851ef43d3c72958fa0d8febc56fe2fbec
Lucas Guilbert <lucas.guilbert15@gmail.com>
2021-12-04 12:06:43 +0100

    add nominal tests

---
+++ test/year_2021/day_4/test.dart
@@ -0,0 +1,53 @@
+import 'package:test/test.dart';
+
+import '../../test_util.dart';
+import '../../../bin/util.dart';
+import '../../../bin/year_2021/day_4/main.dart';
+
+List<Test> testCases1 = [
+  Test(
+      "Nominal example - OK",
+      readStringData("bin/year_2021/day_4/input_example.txt"),
+      resolve1,
+      4512,
+      null),
+  Test("Puzzle - OK", readStringData("bin/year_2021/day_4/input.txt"), resolve1,
+      29440, null)
+];
+
+List<Test> testCases2 = [
+  Test(
+      "Nominal example - OK",
+      readStringData("bin/year_2021/day_4/input_example.txt"),
+      resolve2,
+      1924,
+      null),
+  Test("Puzzle - OK", readStringData("bin/year_2021/day_4/input.txt"), resolve2,
+      13884, null)
+];
+
+void main() {
+  group('Puzzle 1', () {
+    for (Test t in testCases1) {
+      test(t.name, () async {
+        if (t.expectedResult != null) {
+          t.testResult();
+        } else {
+          t.testError();
+        }
+      });
+    }
+  });
+
+  group('Puzzle 2', () {
+    for (Test t in testCases2) {
+      test(t.name, () async {
+        if (t.expectedResult != null) {
+          t.testResult();
+        } else {
+          t.testError();
+        }
+      });
+    }
+  });
+}
