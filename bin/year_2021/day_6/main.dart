9328546c0f36cf71f2502818c0630a78f301716e
GUILBERT Lucas <lucas.guilbert@ecole.ensicaen.fr>
2021-12-06 18:41:10 +0100

    add simpliest solution for day 6

--- bin/year_2021/day_6/main.dart
+++ bin/year_2021/day_6/main.dart
@@ -9,7 +9,7 @@ int main(List<String> args) {
 
   processPuzzle(1, () => resolve1(data));
 
-  processPuzzle(2, () => resolve2(data));
+  processPuzzle(2, () => resolve22(data));
 
   return 0;
 }
@@ -20,6 +20,28 @@ Future<int> resolve1(List<String> data) async {
 
 Future<int> resolve2(List<String> data) async {
   return solve(data, 256);
+}
+
+Future<int> resolve22(List<String> data) async {
+  List<int> lanternFishes = stringListToIntList(data[0].split(","));
+
+  List<int> timers = List.filled(9, 0);
+  for (int i in lanternFishes) {
+    timers[i]++;
+  }
+
+  for (int i = 0; i < 256; i++) {
+    List<int> prev = List.from(timers);
+    for (int j = 0; j < 8; j++) {
+      timers[j] = prev[j + 1];
+    }
+    timers[6] += prev[0];
+    timers[8] = prev[0];
+  }
+
+  print(timers);
+
+  return timers.reduce((value, element) => value + element);
 }
 
 int solve(List<String> data, int max) {
